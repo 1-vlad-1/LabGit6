9bcc7c983be182cc5497d6e59c2d3950523cce46
1-vlad-1 <vlad01022002@mail.ru>
2023-05-15 18:09:39 +0300

    third commit to future

--- code_1.py
+++ code_1.py
@@ -1,6 +1,6 @@
 def capitalize(String):
     return String.title()
-capitalize("shop") # [Shop]
+capitalize("new year") # [New Year]
 capitalize("python programming") # [Python Programming]
 capitalize("how are you!") # [How Are You!]
 
@@ -8,16 +8,16 @@ def merge(dic1,dic2):
     dic3=dic1.copy()
     dic3.update(dic2)
     return dic3
-dic1={1:"hello", 2:"world"}
+dic1={1:"keyboard", 2:"screen"}
 dic2={3:"Python", 4:"Programming"}
-merge(dic1,dic2) # {1: 'hello', 2: 'world', 3: 'Python', 4: 'Programming'}
+merge(dic1,dic2) # {1: 'keyboard', 2: 'screen', 3: 'Python', 4: 'Programming'}
 
 import time # импортируем модуль
 start_time= time.time()
 def fun():
     a=2
     b=3
-    c=a+b
+    c=b * a ** 2 /b
 end_time= time.time()
 fun()
 timetaken = end_time - start_time
